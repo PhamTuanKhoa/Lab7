310017249ff151789aa240c3b3f0cd451c61aff9
Phạm Tuấn Khoa <127456324+PhamTuanKhoa@users.noreply.github.com>
2024-02-03 16:01:42 +0700

    Delete js directory

--- js/login.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("submit-login").addEventListener("click", function() {
-    var username = document.getElementById("username").value;
-    var x = sessionStorage.setItem("username",username)
-});
-
